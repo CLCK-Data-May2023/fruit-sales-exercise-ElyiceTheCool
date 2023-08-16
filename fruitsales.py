54c26f19792e4a6ea4b5487dc5e1c1e61192096b
Alyssia Bunts <alyssiabunts@gmail.com>
2023-08-16 09:24:24 -0400

    Fixed file creattion location. Temporarily

--- fruitsales.py
+++ fruitsales.py
@@ -4,6 +4,4 @@ import pandas as pd
 df = pd.DataFrame({'Apples': [35, 41], 'Bananas': [21, 34]}, index=['2017 Sales', '2018 Sales'])
 
 # Write the data to fruit.csv in project directory
-df.to_csv("./fruit.csv")
-
-# TODO: Write into correct folder!
+df.to_csv("C:/Users/abunt/OneDrive/Documents/VSCode/Code KY/fruit_sales/fruit-sales-exercise-ElyiceTheCool/fruit.csv")
